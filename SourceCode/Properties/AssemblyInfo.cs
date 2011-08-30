38c9984ebbca83208c8deaa90bf77cbf4a180c63
Brandon Stirnaman <stirno@gmail.com>
2011-08-30 08:45:15 -0500

    Updated assembly information.

--- SourceCode/Properties/AssemblyInfo.cs
+++ SourceCode/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FluentWatiN")]
+[assembly: AssemblyTitle("FluentAutomation API")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FluentWatiN")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Brandon Stirnaman")]
+[assembly: AssemblyProduct("FluentAutomation")]
+[assembly: AssemblyCopyright("Copyright © Brandon Stirnaman 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
