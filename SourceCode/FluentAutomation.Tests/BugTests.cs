af3a7beb862b32e6693d1f488a12bf0b9478769b
Brandon Stirnaman <stirno@gmail.com>
2011-09-01 20:28:41 -0500

    Added Test project.

---
+++ SourceCode/FluentAutomation.Tests/BugTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentAutomation.Tests
+{
+    [TestClass]
+    public class BugTests
+    {
+        [TestMethod]
+        public void Bug_4_CantExpectValueOnInput()
+        {
+        }
+    }
+}
