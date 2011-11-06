5ca9993a4dc62913e4ceeabca6d96d7fd986bd2d
Brandon Stirnaman <stirno@gmail.com>
2011-11-05 20:30:56 -0500

    Julian Jelfs DialogHandler for WatiN

---
+++ SourceCode/FluentAutomation.WatiN/JavascriptDialogHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WatiN.Core.DialogHandlers;
+using WatiN.Core.Native.InternetExplorer;
+using WatiN.Core.Native.Windows;
+
+namespace FluentAutomation.WatiN
+{
+    // Borrowed from Julian Jelfs, good stuff!
+    public class JavaScriptAlertDialogHandler : BaseDialogHandler
+    {
+        private readonly Action<string> _onAlert;
+
+        public JavaScriptAlertDialogHandler(Action<string> onAlert)
+        {
+            _onAlert = onAlert;
+        }
+
+        public override bool HandleDialog(Window window)
+        {
+            if (CanHandleDialog(window))
+            {
+                _onAlert(window.Message);
+                new WinButton(GetOKButtonId(), window.Hwnd).Click();
+                return true;
+            }
+            return false;
+        }
+
+        public override bool CanHandleDialog(Window window)
+        {
+            return (window.StyleInHex == "94C801C5" && !ButtonWithId1Exists(window.Hwnd));
+        }
+
+        private static int GetOKButtonId()
+        {
+            return 2;
+        }
+
+        protected static bool ButtonWithId1Exists(IntPtr windowHwnd)
+        {
+            var button = new WinButton(1, windowHwnd);
+            return button.Exists();
+        }
+    }
+
+}
