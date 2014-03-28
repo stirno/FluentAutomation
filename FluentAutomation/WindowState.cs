b5c2848c6c4aad38d88284962d2bd842f976f15f
Brandon Stirnaman <stirno@gmail.com>
2014-03-28 17:33:50 -0500

    Missed WindowState!

---
+++ FluentAutomation/WindowState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentAutomation
+{
+    public class WindowState
+    {
+        public string Source { get; set; }
+
+        public Uri Url { get; set; }
+    }
+}
