a22a5cd81a5c3b3b7ee6a4e7fd768e4bc5c5cf93
Paul Zumbrun <pauljz@gmail.com>
2014-03-31 20:49:02 -0500

    Add Hover and Focus tests

---
+++ FluentAutomation.Tests/Pages/ScrollingPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentAutomation.Tests.Pages
+{
+    public class ScrollingPage : PageObject<ScrollingPage>
+    {
+        public ScrollingPage(FluentTest test)
+            : base(test)
+        {
+            this.Url = "/Scrolling";
+        }
+
+        public string TextSelector = "#bigtext";
+
+        public string BodySelector = "#big";
+
+        public string TopLeftSelector = "#topleft > button";
+
+        public string TopRightSelector = "#topright > button";
+
+        public string BottomLeftSelector = "#bottomleft > button";
+
+        public string BottomRightSelector = "#bottomright > button";
+
+        public string HoverColor = "rgb(255, 0, 0)";
+
+        public string FocusColor = "rgb(0, 0, 255)";
+    }
+}
---
+++ FluentAutomation.Tests/Pages/TextPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentAutomation.Tests.Pages
+{
+    public class TextPage : PageObject<TextPage>
+    {
+        public TextPage(FluentTest test)
+            : base(test)
+        {
+            this.Url = "/Text";
+        }
+
+        public string TitleSelector = "#title";
+
+        public string Paragraph1Selector = "#paragraph1";
+
+        public string Paragraph2Selector = "#paragraph2";
+
+        public string Link1Selector = "#link1";
+
+        public string Link2Selector = "#link1";
+
+        public string HoverColor = "rgb(255, 0, 0)";
+
+        public string FocusColor = "rgb(0, 0, 255)";
+    }
+}
