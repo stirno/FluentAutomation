445f23886ccb91ef103584ef19c45189db6752ba
Onno Valkering <ovalkering@mirabeau.nl>
2015-06-29 14:25:39 +0200

    Added documentation to WbTstr interface

--- FluentAutomation.SeleniumWebDriver/Interfaces/IWbTstrBrowserStackOperatingSystem.cs
+++ FluentAutomation.SeleniumWebDriver/Interfaces/IWbTstrBrowserStackOperatingSystem.cs
@@ -7,10 +7,24 @@ namespace FluentAutomation.Interfaces
 {
     public interface IWbTstrBrowserStackOperatingSystem
     {
+        /// <summary>
+        /// It doesn't matter which operating system is used.
+        /// </summary>
+        /// <returns>Current WbTstr instance</returns>
         IWbTstr IsAny();
 
+        /// <summary>
+        /// Windows is the prefered operating system.
+        /// </summary>
+        /// <param name="version">Optional version number (see BrowserStack documentation)</param>
+        /// <returns>Current WbTstr instance</returns>
         IWbTstr IsWindows(string version = null);
 
+        /// <summary>
+        /// OS X is the prefered operating system.
+        /// </summary>
+        /// <param name="version">Optional version number (see BrowserStack documentation)</param>
+        /// <returns></returns>
         IWbTstr IsOSX(string version = null);
     }
 }
