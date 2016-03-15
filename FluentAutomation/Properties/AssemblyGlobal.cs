0e5475ec0b1bf90cc4dae195ef5d01036a274544
Sean McCarthy <scmccart@gmail.com>
2016-03-15 11:27:27 -0400

    Trying out the fix @predeekc offers for issue #132.

--- FluentAutomation/Properties/AssemblyGlobal.cs
+++ FluentAutomation/Properties/AssemblyGlobal.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
