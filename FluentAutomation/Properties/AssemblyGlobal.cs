b652c044c4e1bfb2aa09cfb70c0ac70ee4e716ab
tiandx <dorothy.tian@lexisnexis.com>
2016-11-02 18:19:23 +0800

    Update version number to align with our NuGet.

--- FluentAutomation/Properties/AssemblyGlobal.cs
+++ FluentAutomation/Properties/AssemblyGlobal.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.0.7.0")]
